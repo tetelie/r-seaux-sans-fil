b2811297f1ed93f86814fc4edcd22e6862e56d14
Σlie * <44349276+tetelie@users.noreply.github.com>
2025-02-21 09:41:26 +0100

    add serveur.rs

---
+++ rsf/src/serveur.rs
@@ -0,0 +1,43 @@
+use std::fmt::format;
+use std::fs::File;
+use std::io::{Read,Write};
+use std::net::TcpListener;
+use std::process::Command;
+
+fn main() -> std::io::Result<()>{
+    let listener = TcpListener::bind("0.0.0.0:7878");
+    println!("Serveur en attente de connexion...");
+
+    for stream in listener?.incoming(){
+        let mut stream = stream?;
+        println!("Connexion acceptée!");
+
+        // Lire le nom du fichier
+        let mut file_name_buf = [0; 128];
+        stream.read_exact(&mut file_name_buf)?;
+        let file_name = String::from_utf8_lossy(&file_name_buf).trim_end_matches('\0').to_string();
+
+        // Sauvgarde du fichier
+        let mut file= File::create(&file_name)?;
+        let mut buffer = Vec::new();
+        stream.read_to_end(&mut buffer)?;
+        file.write_all(&buffer)?;
+        println!("Fichier reçu : {}", file_name);
+
+        // Rendre le fichier éxécutable
+        let _ = Command::new("chmod").arg("+x").arg(&file_name).output();
+
+        // Executer le fichier
+        let output = Command::new(format!("./{}", file_name)).output().expect("Echec de l'execution du fichier");
+
+        println!("Sortie du programme : {}", String::from_utf8_lossy(&output.stdout));
+
+    }
+
+    Ok(())
+
+
+
+
+
+}
